d782541665891943af7d776de24b4382a8eb5d7b
Frank Vinci <fv42wid@yahoo.com>
2019-01-21 15:57:41 -0500

    create post done, fix date then follows

--- src/main/java/spritter/controllers/TweetController.java
+++ src/main/java/spritter/controllers/TweetController.java
@@ -1,25 +1,61 @@
 package spritter.controllers;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.SessionAttributes;
 import org.springframework.web.bind.support.SessionStatus;
+
+import javax.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
 
 import lombok.extern.slf4j.Slf4j;
 
 import spritter.data.User;
+import spritter.data.Post;
+import spritter.data.PostRepository;
 
 @Slf4j
 @Controller
 @RequestMapping(value={"/", "/tweets"})
 public class TweetController {
 	
+	private PostRepository postRepo;
+	
+	public TweetController(PostRepository postRepo) {
+		this.postRepo = postRepo;
+	}
+	
 	@GetMapping
-	public String showTweets(SessionStatus sessionStatus, @AuthenticationPrincipal User user) {
+	public String showTweets(SessionStatus sessionStatus, @AuthenticationPrincipal User user, Model model) {
+		model.addAttribute("post", new Post());
+		List<User> users = new ArrayList<User>() {{ 
+			add(user);
+		}};
+		
+		List<Post> posts = postRepo.findPostsByUserIn(users);
+		
+		model.addAttribute("newUsers", users);
+		model.addAttribute("posts", posts);
 		return "tweets/index";
+	}
+	
+	@PostMapping
+	public String createTweet(@Valid Post post, BindingResult result, SessionStatus sessionStatus, @AuthenticationPrincipal User user) {
+		if(result.hasErrors()) {
+			log.info("Post - " + result);
+			return "tweets/index";
+		}
+		post.setUser(user);
+		postRepo.save(post);
+		return "redirect:/tweets";
 	}
 
 }
--- src/main/java/spritter/data/PostRepository.java
+++ src/main/java/spritter/data/PostRepository.java
@@ -1,9 +1,13 @@
 package spritter.data;
 
+import java.util.Collection;
+import java.util.List;
+
 import org.springframework.data.repository.CrudRepository;
 
 import spritter.data.Post;
 
 public interface PostRepository extends CrudRepository<Post, Long> {
 
+	List<Post> findPostsByUserIn(Collection<User> users);
 }
